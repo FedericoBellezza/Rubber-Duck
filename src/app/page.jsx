9a6716e3f49719b5394d7cee43ffa0a3245529ed
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-05-07 11:45:42 +0200

    add newsletter section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Characteristics from "@/components/Characteristics";
 import DuckCollection from "@/components/DuckCollection";
 import FAQs from "@/components/FAQs";
 import Hero from "@/components/Hero";
+import NewsLetter from "@/components/NewsLetter";
 import Reviews from "@/components/Reviews";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Reviews />
       <DuckCollection />
       <FAQs />
+      <NewsLetter />
     </div>
   );
 }
---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,44 @@
+export default function NewsLetter() {
+  return (
+    <section className="flex flex-col gap-10 items-center py-20 bg-gray-100">
+      <h2 className="text-6xl font-bold">Iscriviti per rimanere aggiornato</h2>
+      <span>
+        Iscriviti alla nostra newsletter per rimanere aggiornato su sconti e
+        novità
+      </span>
+      <form className="w-1/5" action="">
+        <div className="flex">
+          <input
+            name="email"
+            type="email"
+            className="p-2 grow active:outline-none focus:outline-none rounded-xl"
+            placeholder="Lascia la tua mail qui..."
+          />
+          <button className=" text-green-500 hover:text-green-600 transition cursor-pointer w-15  h-15 relative overflow-hidden">
+            <svg className="h-15 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <use href="#send"></use>
+            </svg>
+          </button>
+        </div>
+        <hr />
+      </form>
+      <div className="flex gap-10">
+        <svg className="w-13 h-13 text-green-500 hover:text-green-600 transition cursor-pointer border rounded-full p-2">
+          <use href="#facebook"></use>
+        </svg>
+        <svg className="w-13 h-13 text-green-500 hover:text-green-600 transition cursor-pointer border rounded-full p-2">
+          <use href="#twitter"></use>
+        </svg>
+        <svg className="w-13 h-13 text-green-500 hover:text-green-600 transition cursor-pointer border rounded-full p-2">
+          <use href="#youtube"></use>
+        </svg>
+        <svg className="w-13 h-13 text-green-500 hover:text-green-600 transition cursor-pointer border rounded-full p-2">
+          <use href="#linkedin"></use>
+        </svg>
+        <svg className="w-13 h-13 text-green-500 hover:text-green-600 transition cursor-pointer border rounded-full p-2">
+          <use href="#instagram"></use>
+        </svg>
+      </div>
+    </section>
+  );
+}
