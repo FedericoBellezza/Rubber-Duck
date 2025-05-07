f37c729634ee03cabca1dbd6fb9d80463fb95865
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-05-07 15:10:48 +0200

    add responsive hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 export default function Hero() {
   return (
-    <section id="home" className="flex">
-      <div className="w-1/2 aspect-7/5 overflow-hidden bg-[url('/banner-image.png')] bg-center "></div>
-      <div className="w-1/2 flex flex-col gap-10 items-center justify-center p-10 bg-[url('/banner-bg.png')] text-gray-700 ">
-        <h1 className="text-8xl font-bold text-center  ">
+    <section id="home" className="flex flex-col lg:flex-row">
+      <div className="lg:w-1/2 h-100 lg:aspect-7/5 overflow-hidden bg-[url('/banner-image.png')] bg-cover lg:h-max  bg-center "></div>
+      <div className="lg:w-1/2 flex flex-col gap-10 items-center justify-center p-10 bg-[url('/banner-bg.png')] text-gray-700 ">
+        <h1 className="lg:text-8xl text-4xl font-bold text-center  ">
           Rubber Ducks <br /> Debuggings
         </h1>
-        <p className="text-center text-3xl text-balance">
+        <p className="text-center lg:text-3xl text-xl text-balance">
           Spiegare il codice ad una paperella di gomma aiuta i programmatori a
           chiarire il pensiero e individuare pensieri nascosti.
         </p>
-        <button className="border p-5 text-3xl cursor-pointer hover:bg-gray-800 hover:text-white transition hover:border-gray-800">
+        <button className="border border-gray-800 p-5 text-xl lg:text-3xl cursor-pointer hover:bg-gray-800 hover:text-white transition ">
           ACQUISTALA SUBITO!
         </button>
       </div>
