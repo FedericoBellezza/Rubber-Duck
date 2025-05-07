9e96b4d3a3669ff5cadb0c1e80a3842fa79ccbe5
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-05-07 15:15:13 +0200

    add responsive to characteristic section

--- src/components/Characteristics.jsx
+++ src/components/Characteristics.jsx
@@ -2,65 +2,69 @@ export default function Characteristics() {
   return (
     <section
       id="characteristics"
-      className="flex justify-between gap-10 p-10 text-balance  my-20 px-30"
+      className="flex flex-col lg:flex-row justify-between gap-10 p-10 text-balance my-10  lg:my-20 px-10 lg:px-30"
     >
-      <div className="flex flex-col gap-20  justify-between items-center w-1/4">
+      <div className="flex flex-col gap-10 lg:gap-20  justify-between items-center lg:w-1/4">
         <div>
-          <h2 className="text-4xl font-bold text-center ">
+          <h2 className="lg:text-4xl text-xl font-bold text-center ">
             Lunga durata della batteria
           </h2>
-          <p className="text-center text-xl">
+          <p className="text-center text-md lg:text-xl">
             Goditi ore di assistenza continua nella risoluzione dei bug grazie
             alla lunga durata della "batteria" (ovvero, la tua pazienza) della
             paperella di gomma.
           </p>
         </div>
         <div>
-          <h2 className="text-4xl font-bold text-center">
+          <h2 className="lg:text-4xl text-xl font-bold text-center">
             Compatibilità universale
           </h2>
-          <p className="text-center text-xl">
+          <p className="text-center text-md lg:text-xl">
             Funziona con tutti i linguaggi di programmazione e sistemi
             operativi, la paperella di gomma è l'unico strumento di debugging
             che non necessita di aggiornamenti.
           </p>
         </div>
         <div>
-          <h2 className="text-4xl font-bold text-center">Design elegante</h2>
-          <p className="text-center text-xl">
+          <h2 className="lg:text-4xl text-xl font-bold text-center">
+            Design elegante
+          </h2>
+          <p className="text-center text-md lg:text-xl">
             Mostra con orgoglio il design elegante e raffinato della tua
             paperella di gomma, disponibile in vari colori per abbinarsi al tuo
             stile di programmatore.
           </p>
         </div>
       </div>
-      <div className="flex justify-around items-center w-1/4">
+      <div className="flex justify-around items-center lg:w-1/4">
         <img src="/invisible.png" alt="" />
       </div>
-      <div className="flex flex-col justify-between items-center w-1/4">
+      <div className="flex flex-col gap-10 lg:gap-20 justify-between items-center lg:w-1/4">
         <div>
-          <h2 className="text-4xl font-bold text-center">
+          <h2 className="lg:text-4xl text-xl font-bold text-center">
             Resistenza agli spruzzi
           </h2>
-          <p className="text-center text-xl">
+          <p className="text-center text-md lg:text-xl">
             Perfetta per resistere ai piccoli incidenti con caffè e bevande, la
             tua paperella di gomma non teme schizzi, mantenendoti sempre pronto
             per il debugging.
           </p>
         </div>
         <div>
-          <h2 className="text-4xl font-bold text-center">Portabilità</h2>
-          <p className="text-center text-xl">
+          <h2 className="lg:text-4xl text-xl font-bold text-center">
+            Portabilità
+          </h2>
+          <p className="text-center text-md lg:text-xl">
             Leggera e facile da trasportare, la paperella di gomma può essere il
             tuo compagno di lavoro ovunque tu vada. Portala con te per un
             supporto costante.
           </p>
         </div>
         <div>
-          <h2 className="text-4xl font-bold text-center">
+          <h2 className="lg:text-4xl text-xl font-bold text-center">
             Promemoria anti-stress
           </h2>
-          <p className="text-center text-xl">
+          <p className="text-center text-md lg:text-xl">
             Quando lo stress del codice diventa troppo, spremi la tua paperella
             di gomma per un rapido sollievo anti-stress, senza bidogno di
             software complicati.
