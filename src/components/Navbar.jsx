e1ed12fe979ca38e8d92d8f6b6e6356e6c185d1a
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-05-07 14:37:05 +0200

    add navbar link changing color on page scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,52 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Navbar() {
+  const [activeSection, setActiveSection] = useState(null);
+  const handleScroll = () => {
+    // console.log("finestra: " + window.scrollY);
+
+    if (
+      document.getElementById("newsletter").offsetTop - 700 <
+      window.scrollY
+    ) {
+      setActiveSection("newsletter");
+    } else if (
+      document.getElementById("faqs").offsetTop - 600 <
+      window.scrollY
+    ) {
+      setActiveSection("faqs");
+    } else if (
+      document.getElementById("collection").offsetTop - 500 <
+      window.scrollY
+    ) {
+      setActiveSection("collection");
+    } else if (
+      document.getElementById("reviews").offsetTop - 500 <
+      window.scrollY
+    ) {
+      setActiveSection("reviews");
+    } else if (
+      document.getElementById("characteristics").offsetTop - 500 <
+      window.scrollY
+    ) {
+      setActiveSection("characteristics");
+    } else if (
+      document.getElementById("home").offsetTop - 500 <
+      window.scrollY
+    ) {
+      setActiveSection("home");
+    }
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  };
+  window.addEventListener("scroll", handleScroll);
+
+  // handleScroll();
+
+  // function to scroll to a section
   function scrollToItem(itemId) {
     const element = document.getElementById(itemId);
     const posizione = element.offsetTop - 100;
@@ -18,37 +64,62 @@ export default function Navbar() {
       />
       <ul className="flex text-lg gap-10 text-gray-500">
         <li
-          className="cursor-pointer hover:text-slate-700"
+          className={`cursor-pointer transition  ${
+            activeSection === "home"
+              ? "text-blue-600 hover:text-blue-800"
+              : "hover:text-slate-700"
+          } `}
           onClick={() => scrollToItem("home")}
         >
           Home
         </li>
+
         <li
-          className="cursor-pointer hover:text-slate-700"
+          className={`cursor-pointer transition  ${
+            activeSection === "characteristics"
+              ? "text-blue-600 hover:text-blue-800"
+              : "hover:text-slate-700"
+          } `}
           onClick={() => scrollToItem("characteristics")}
         >
           I punti di forza
         </li>
         <li
-          className="cursor-pointer hover:text-slate-700"
+          className={`cursor-pointer transition  ${
+            activeSection === "reviews"
+              ? "text-blue-600 hover:text-blue-800"
+              : "hover:text-slate-700"
+          } `}
           onClick={() => scrollToItem("reviews")}
         >
           Recensioni
         </li>
         <li
-          className="cursor-pointer hover:text-slate-700"
+          className={`cursor-pointer transition  ${
+            activeSection === "collection"
+              ? "text-blue-600 hover:text-blue-800"
+              : "hover:text-slate-700"
+          } `}
           onClick={() => scrollToItem("collection")}
         >
           Peperelle
         </li>
         <li
-          className="cursor-pointer hover:text-slate-700"
+          className={`cursor-pointer transition  ${
+            activeSection === "faqs"
+              ? "text-blue-600 hover:text-blue-800"
+              : "hover:text-slate-700"
+          } `}
           onClick={() => scrollToItem("faqs")}
         >
           FAQs
         </li>
         <li
-          className="cursor-pointer hover:text-slate-700"
+          className={`cursor-pointer transition  ${
+            activeSection === "newsletter"
+              ? "text-blue-600 hover:text-blue-800"
+              : "hover:text-slate-700"
+          } `}
           onClick={() => scrollToItem("newsletter")}
         >
           Newsletter
